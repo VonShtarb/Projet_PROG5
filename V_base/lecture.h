11f62c80aa8a117a35e1c87c17463a2855cee12f
abseth2 <58178291+abseth2@users.noreply.github.com>
2019-12-13 13:58:56 +0100

    Add files via upload

---
+++ V_base/lecture.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <elf.h>
+
+// Affiche le header
+void print_header( Elf64_Ehdr elfulu);
+// Recupere le header
+Elf64_Ehdr load_header(char* name);
+// Recupere la table des section
+Elf64_Shdr load_elf_sec(char *file,int sec);
+// Recupere la table des symboles
+Elf64_Shdr load_symb(char *file, Elf64_Ehdr header);
+int main_section(char* name_file, int nb);
+int main_load_header(char* name_file );
