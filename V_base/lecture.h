199e9f40cd00255873af76778c6dc38d55034227
abseth2 <58178291+abseth2@users.noreply.github.com>
2019-12-13 13:58:28 +0100

    Delete lecture.h

--- V_base/lecture.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <elf.h>
-
-void print_header( Elf64_Ehdr elfulu);
-
-Elf64_Ehdr readheader(FILE* f);
-Elf64_Shdr load_elf_sec(char *file,int sec);
-int main_readheader(char* name_file );
