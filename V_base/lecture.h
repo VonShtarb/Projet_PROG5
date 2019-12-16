41560fe4f9c63acda0ffd39a65531c19649fc3b0
VonShtarb <edouard.bernard.07@gmail.com>
2019-12-16 09:19:00 +0100

    tete vide

--- V_base/lecture.h
+++ V_base/lecture.h
@@ -7,3 +7,8 @@ void print_header( Elf64_Ehdr elfulu);
 Elf64_Ehdr readheader(FILE* f);
 Elf64_Shdr load_elf_sec(char *file,int sec);
 int main_readheader(char* name_file );
+void getheaders(char* filename, Elf64_Ehdr header, Elf64_Shdr sectionheader[]);
+Elf64_Shdr read_section_table(char* filename, int offset);
+void get_section_table(char* filename, Elf64_Ehdr header, Elf64_Shdr sectionheaders[]);
+void print_section_table(Elf64_Shdr sectionheaders[], int size);
+int main_read_section_table(char* filename);
